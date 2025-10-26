5d65a3a62a03b37497c7e87921270fb5ca128c17
würthless elektroniks <wurthlesselektroniks@proton.me>
2025-10-26 16:19:43 -0400

    converter overhaul: convert glitch2, prep for zephyr

--- convert_rgh13.py
+++ convert_rgh13.py
@@ -18,6 +18,8 @@ from rc4 import RC4
 SHA1_FALCON_RGH3_27MHZ = "9b89a53dbdb4735782e92b70bb5e956f6b35da5f"
 SHA1_FALCON_RGH3_10MHZ = "a4ae6a6f1ff374739d1c91f8a2881f4eecc210d3"
 
+# TODO: calc this hash
+SHA1_CBB_4577_XEBUILD = ""
 
 SHA1_CBB_5772_XEBUILD = "3a8fb9580ce01cf1c0e2d885e0fd96a05571643f"
 SHA1_CBB_6752_XEBUILD = "899cd01e00ef7b27ceb010dde42e4d6e9c871330"
@@ -35,14 +37,14 @@ def _parse_cpukey(cpu_key):
 def _init_argparser():
     argparser = ArgumentParser(formatter_class=RawTextHelpFormatter,
                                prog='convert_rgh13',
-                               description='Converts RGH3 NAND to RGH1.3/EXT+3')
+                               description='Converts RGH1.2 or RGH3 NAND to RGH1.3/EXT+3')
     
     argparser.add_argument("--board",
-                           help="Specifies target board (xenon, zephyr, falcon, jasper). Required for some CB_B versions.")
+                           help="Specifies target board (xenon, elpis, zephyr, falcon, jasper). Required for some CB_B versions.")
 
     argparser.add_argument("--cpukey",
                            type=_parse_cpukey,
-                           help="Specify CPU key, required to run older dashboards")
+                           help="Specify CPU key (required for Glitch2 images or to run older kernels)")
     
     argparser.add_argument("--fast5050",
                            default=False,
@@ -79,7 +81,6 @@ def _init_argparser():
                            action='store_true',
                            help="Use one-wire POST method")
 
-
     argparser.add_argument("--zerowire",
                            default=False,
                            action='store_true',
@@ -99,29 +100,39 @@ SMC_FILEPATH_MAP = {
 
     'falcon_tiltsw': os.path.join("smc", "build", "rgh13_jasper_for_falcon.bin"),
     'badfalcon_tiltsw': os.path.join("smc", "build", "rgh13_badjasper_for_falcon.bin"),
+    'zephyr_tiltsw': os.path.join("smc", "build", "rgh13_jasper_for_falcon.bin"),
+    'badzephyr_tiltsw': os.path.join("smc", "build", "rgh13_badjasper_for_falcon.bin"),
     'jasper_tiltsw': os.path.join("smc", "build", "rgh13_jasper.bin"),
     'badjasper_tiltsw': os.path.join("smc", "build", "rgh13_badjasper.bin"),
 
     'falcon_extpwr': os.path.join("smc", "build", "rgh13_jasper_for_falcon_extpwr.bin"),
     'badfalcon_extpwr': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_extpwr.bin"),
+    'zephyr_extpwr': os.path.join("smc", "build", "rgh13_jasper_for_falcon_extpwr.bin"),
+    'badzephyr_extpwr': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_extpwr.bin"),
     'jasper_extpwr': os.path.join("smc", "build", "rgh13_jasper_extpwr.bin"),
     'badjasper_extpwr': os.path.join("smc", "build", "rgh13_badjasper_extpwr.bin"),
 
     'falcon_chkstop': os.path.join("smc", "build", "rgh13_jasper_for_falcon_chkstop.bin"),
     'badfalcon_chkstop': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_chkstop.bin"),
+    'zephyr_chkstop': os.path.join("smc", "build", "rgh13_jasper_for_falcon_chkstop.bin"),
+    'badzephyr_chkstop': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_chkstop.bin"),
     'jasper_chkstop': os.path.join("smc", "build", "rgh13_jasper_chkstop.bin"),
     'badjasper_chkstop': os.path.join("smc", "build", "rgh13_badjasper_chkstop.bin"),
 
     'xenon_1wire':  os.path.join("smc", "build", "rgh13_xenon_1wire.bin"),
     'falcon_1wire': os.path.join("smc", "build", "rgh13_jasper_for_falcon_1wire.bin"),
+    'zephyr_1wire': os.path.join("smc", "build", "rgh13_jasper_for_falcon_1wire.bin"),
     'jasper_1wire': os.path.join("smc", "build", "rgh13_jasper_1wire.bin"),
     'badfalcon_1wire': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_1wire.bin"),
+    'badzephyr_1wire': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_1wire.bin"),
     'badjasper_1wire': os.path.join("smc", "build", "rgh13_badjasper_1wire.bin"),
 
     'xenon_0wire':  os.path.join("smc", "build", "rgh13_xenon_0wire.bin"),
     'falcon_0wire': os.path.join("smc", "build", "rgh13_jasper_for_falcon_0wire.bin"),
+    'zephyr_0wire': os.path.join("smc", "build", "rgh13_jasper_for_falcon_0wire.bin"),
     'jasper_0wire': os.path.join("smc", "build", "rgh13_jasper_0wire.bin"),
     'badfalcon_0wire': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_0wire.bin"),
+    'badzephyr_0wire': os.path.join("smc", "build", "rgh13_badjasper_for_falcon_0wire.bin"),
     'badjasper_0wire': os.path.join("smc", "build", "rgh13_badjasper_0wire.bin"),
 }
 
@@ -130,9 +141,51 @@ def encrypt_cba(cba, rnd = "CB_ACB_ACB_ACB_A"):
     key = hmac.new(key_1BL, rnd, hashlib.sha1).digest()[0:0x10]
     return (key, cba[0:0x10] + rnd + RC4(key).crypt(cba[0x20:]))
 
+decrypt_cba = encrypt_cba
+
 def encrypt_cbb(cbb, cba_key, rnd=b"CB_BCB_BCB_BCB_B", cpu_key=b"\x00"*16):
     key = hmac.new(cba_key, rnd + cpu_key, hashlib.sha1).digest()[0:0x10]
     return cbb[0:0x10] + rnd + RC4(key).crypt(cbb[0x20:])
+
+decrypt_cbb = encrypt_cbb
+
+def _extract_loaders(nand_stripped: bytes) -> dict:
+    pos = 0x8000
+    result = {}
+
+    while True:
+        loader_type_raw = nand_stripped[pos+0x01]
+        loader_size = struct.unpack(">I", nand_stripped[pos+0x0C:pos+0x010])[0]
+
+        # pad CE to nearest 16th byte boundary
+        if (loader_size & 0x0F) != 0:
+            if loader_type_raw != 0x45:
+                raise RuntimeError("non-CE loader not 16-byte aligned")
+            loader_size = (loader_size + 0x10) & 0xFFFFFFF0
+
+        loader      = nand_stripped[pos:pos+loader_size]
+
+        if loader_type_raw == 0x42:
+            if loader_size == 0x400 and 'cbx' not in result:
+                result['cbx'] = loader
+            elif 'cba' not in result:
+                result['cba'] = loader
+            elif 'cbb' not in result:
+                result['cbb'] = loader
+            else:
+                raise RuntimeError("unrecognized CB found in bootloader chain")
+        elif loader_type_raw == 0x44:
+            if 'cd' in result:
+                raise RuntimeError("found more than one CD")
+            result['cd'] = loader
+        elif loader_type_raw == 0x45:
+            result['ce'] = loader
+
+            # xeBuild leaves empty space between CE and XeLL
+            return result
+        else:
+            print(f"unrecognized loader type encountered: 0x{loader_type_raw:02x}")
+        pos += loader_size
 
 def main():
     argparser = _init_argparser()
@@ -151,7 +204,6 @@ def main():
         print("NAND type cannot be detected, exiting.")
         return
     
-    # FIXME: there is a bug somewhere that breaks big block NANDs, have to find it...
     if nand_type == ecc.NandType.NAND_64M:
         print("found Jasper big boy NAND")
 
@@ -161,8 +213,7 @@ def main():
     if nand_type == ecc.NandType.NAND_16M:
         print("found standard 16m NAND")
 
-    # only inspect/modify the first chunk (1 block on big blocks)
-    nand_stripped = ecc.ecc_strip(nand[0:0x021000])
+    nand_stripped = ecc.ecc_strip(nand[0:0x021000 * 4])
 
     # the SMC data MUST be from 0x1000~0x4000, there's no microsoft SMC that isn't like that
     if nand_stripped[0x78:0x80] != bytes([0x00, 0x00, 0x30, 0x00, 0x00, 0x00, 0x10, 0x00]):
@@ -178,54 +229,75 @@ def main():
     else:
         print(f"WARNING: unrecognized SMC: {smc_hash.hexdigest()}")
 
-    # now identify if this really is a RGH3 image.
-    # find first CB @ 0x8000
-    # then look at next CB in chain
-    # if it's not CB_X (ver 15432), then it's not an RGH3 image
-    cba_addr = 0x8000
-    cba_size = struct.unpack(">I", nand_stripped[0x800C:0x8010])[0]
-    next_cb_addr = cba_addr+cba_size
-    if nand_stripped[next_cb_addr:next_cb_addr+4] != bytes([0x43, 0x42, 0x3C, 0x48]):
-        print("error: not a RGH3 image - CB_X did not follow CB_A")
+    # detect XeLL position
+    xell_pos = None
+    xell_signature = bytes([0x48, 0x00, 0x00, 0x20, 0x48, 0x00, 0x00, 0xEC,
+                            0x48, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00,
+                            0x48, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00,
+                            0x48, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00])
+
+    if nand_stripped[0x070000:0x070000+len(xell_signature)] == xell_signature:
+        xell_pos = 0x070000
+    elif nand_stripped[0x73800:0x73800+len(xell_signature)] == xell_signature:
+        xell_pos = 0x73800
+
+    if xell_pos is None:
+        print("error: cannot find XeLL - might not be a valid glitch2/glitch3 image")
+        return
+    
+    max_loaderspace_size = xell_pos-0x8000
+    print(f"XeLL found at 0x{xell_pos:03x}. estimated bootloader space is {max_loaderspace_size} byte(s)")
+    
+    loaders = _extract_loaders(nand_stripped)
+
+    imagetype = None
+    if 'cbx' in loaders:
+        print("found glitch3/RGH3 image")
+        imagetype = 3
+    elif 'cbb' in loaders:
+        if args.cpukey is None:
+            print("error: glitch2 image detected - you must specify CPU key")
+            return
+        print("found glitch2/RGH1.2 image")
+        imagetype = 2
+    else:
+        print("error: unrecognized image type. stopping.")
         return
 
-    print("image looks like RGH3 or glitch3, checking CB_B")
+    cba_seed = loaders['cba'][0x10:0x20]
+    cba_key, _ = decrypt_cba(loaders['cba'], rnd=cba_seed)
+    if imagetype == 2:
+        cbb = decrypt_cbb(loaders['cbb'], cba_key, loaders['cbb'][0x10:0x20], cpu_key=args.cpukey)
+    else:
+        cbb = loaders['cbb'] # Glitch3 already has that in plaintext
 
-    # advance to next CB stage
-    cbx_size = struct.unpack(">I", nand_stripped[next_cb_addr+0x0C:next_cb_addr+0x10])[0]
-    cbx_seed = nand_stripped[next_cb_addr+0x10:next_cb_addr+0x20]
-    next_cb_addr += cbx_size
-    if nand_stripped[next_cb_addr:next_cb_addr+2] != bytes([0x43, 0x42]):
-        print("error: CB magicword not found at next CB slot")
-        return
-
-    cbb_version = struct.unpack(">H",nand_stripped[next_cb_addr+2:next_cb_addr+4])[0]
-    cbb_size = struct.unpack(">I", nand_stripped[next_cb_addr+0x0C:next_cb_addr+0x10])[0]
-
-    cbb = nand_stripped[next_cb_addr:next_cb_addr+cbb_size]
+    cbb_version = struct.unpack(">H",cbb[2:4])[0]
+    cbb_size = struct.unpack(">I", cbb[0x0C:0x10])[0]
 
     # calc SHA hash of CB_B to determine what it really is
     # (hash runs from 0x140 through to end of CB_B)
     cbb_hash = hashlib.sha1(cbb[0x140:]).hexdigest()
 
-    #with open("cbb_debug.bin", "wb") as f:
-    #    f.write(cbb)
-
     smctype = ""
 
+    if cbb_version == 4577 and cbb_hash == SHA1_CBB_4577_XEBUILD:
+        print("found xeBuild-patched Zephyr CB_B")
+        smctype = "zephyr"
+        
     if cbb_version == 5772 and cbb_hash == SHA1_CBB_5772_XEBUILD:
         print("found xeBuild-patched Falcon CB_B")
 
         # J-Runner's annoying Falcon-for-Xenon behavior forces us to do this
         if args.board is None:
-            print("error: ambiguous target board. please specify --board xenon or --board falcon")
+            print("error: ambiguous target board. please specify --board xenon, --board elpis or --board falcon")
             return
         
-        if args.board not in [ "xenon", "falcon" ]:
+        if args.board not in [ "xenon", "falcon", "elpis" ]:
             print("error: 5772 should only be present on falcon and falcon-for-xenon builds")
             return
 
-        smctype = args.board
+        # xenon/elpis are the same SMC type
+        smctype = args.board if args.board != "elpis" else "xenon"
 
     elif cbb_version == 6752 and cbb_hash == SHA1_CBB_6752_XEBUILD:
         print("found xeBuild-patched Jasper CB_B")
@@ -235,8 +307,13 @@ def main():
             return
 
         smctype = "jasper"
+
+    elif cbb_version == 7378 and cbb_hash == SHA1_CBB_7378_ELPISS:
+        print("found elpiss hacked 7378 CB_B")
+        smctype = "xenon"
     else:
         print(f"error: unrecognized/unsupported CB_B version (hash was: {cbb_hash})")
+        print("possible issues: image already patched, CB_B unsupported, invalid CPU key")
         return
 
     if args.onewire:
@@ -285,27 +362,18 @@ def main():
     nand_stripped[0x1000:0x4000] = smcdata_encrypted
     print("injected new SMC program")
 
+    new_loader_buffer = bytearray([])
+
+
     # replace whatever CB_A is there with the 9188 MFG image
     new_cba = None
-
     cba_file = "cba_9188_mfg.bin" if args.cpukey is None else "cba_5772.bin"
     with open(os.path.join("cba", cba_file), "rb") as f:
         new_cba = f.read()
-    if len(new_cba) > cba_size:
-        print("error: replacement CB_A somehow larger than the original")
-        return
-
-    padding_bytes_required = cba_size - len(new_cba)
-    print(f"CB_B will be padded by {padding_bytes_required} byte(s)")
-
-    hmac_seed = nand_stripped[cba_addr+0x10:cba_addr+0x20]
-    cba_key, cba_encrypted = encrypt_cba(new_cba, rnd=hmac_seed)
-
-    cb_inject_pos = 0x8000
-    nand_stripped[cb_inject_pos:cb_inject_pos+len(cba_encrypted)] = cba_encrypted
-    cb_inject_pos += len(cba_encrypted)
-
-    print("injected CB_A 9188 MFG")
+    print(f"loaded CB_A from file: {cba_file}")
+    
+    cba_key, cba_encrypted = encrypt_cba(new_cba, rnd=cba_seed)
+    new_loader_buffer += cba_encrypted
 
     # drop CB_Y into place
     new_cbx = None
@@ -316,19 +384,21 @@ def main():
     if len(new_cbx) != 0x400:
         print("error: CB_X/CB_Y was not 0x400 bytes")
 
+    cbx_seed = loaders['cbx' if 'cbx' in loaders else 'cbb'][0x10:0x20]
+
     if cbx_file == "cbx_xell.bin":
         new_cbx = bytearray(new_cbx)
         new_cbx[0x3C0:0x3C4] = bytes([0x7F, 0xE4, 0xFB, 0x78]) # mov r4,r31 (avoid r31 being trashed by cbb_jump)
         new_cbx, _ = assemble_branch(new_cbx, 0x3C4, 0x478) # jump to CB_A cbb_jump function
+
+    print(f"loaded CB_X/CB_Y from file: {cbx_file}")
 
     if args.cpukey is None:
         cby_encrypted = encrypt_cbb(new_cbx, cba_key, rnd=cbx_seed)
     else:
         cby_encrypted = encrypt_cbb(new_cbx, cba_key, rnd=cbx_seed, cpu_key=args.cpukey)
 
-    nand_stripped[cb_inject_pos:cb_inject_pos+len(cby_encrypted)] = cby_encrypted
-    cb_inject_pos += len(cby_encrypted)
-    print("injected CB_X/CB_Y")
+    new_loader_buffer += cby_encrypted
 
     # inject appropriate hacked CB_B
     cbb_patched = rgh13cbb_do_patches(cbb, use_smc_ipc=args.onewire or args.zerowire)
@@ -363,12 +433,20 @@ def main():
         else:
             print("fast5050: CB_B unrecognized, no patches applied.")
         
-    cbb_patched += bytes([0x00 * padding_bytes_required])
-    cbb_patched[0x000C:0x0010] = struct.pack(">I", cbb_size + padding_bytes_required)
+    new_loader_buffer += cbb_patched
+    new_loader_buffer += loaders['cd']
+    new_loader_buffer += loaders['ce']
 
-    nand_stripped[cb_inject_pos:cb_inject_pos+len(cbb_patched)] = cbb_patched
-    print("patched and padded CB_B")
-    
+    if len(new_loader_buffer) > max_loaderspace_size:
+        print(f"error: maximum bootloader size exceeded. maximum was {max_loaderspace_size}, got {len(new_loader_buffer)}")
+        return
+    zeropad_size = max_loaderspace_size-len(new_loader_buffer)
+    print(f"built new loader space successfully. total was {len(new_loader_buffer)}, with {zeropad_size} byte(s) to spare")
+    new_loader_buffer += bytes([0] * zeropad_size)
+
+    nand_stripped[0x8000:0x8000+max_loaderspace_size] = new_loader_buffer
+    print("injected patched loader chain successfully!")
+
     print("recalculating ECC data...")
     nand_stripped_ecc = ecc.ecc_encode(nand_stripped, nand_type, 0)
     nand = bytearray(nand)
