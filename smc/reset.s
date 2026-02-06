b9b172f8bc309788d82763e6ea3424f1afdcee1d
würthless elektroniks <wurthlesselektroniks@proton.me>
2026-02-06 09:07:22 -0500

    retry 5 times on rrod. should fix occasional hwinit false positives

--- smc/reset.s
+++ smc/reset.s
@@ -5,10 +5,18 @@
 
 ;
 ; hook arrives from normal reset watchdog code and indicates that
-; a RRoD error case happened, so raise it immediately instead of
-; doing five tries then giving up
+; a RRoD error case happened. we should already have patched the normal
+; "give up on timeout" code to land in reset_via_watchdog below, so this
+; handles "real" errors.
 ;
 on_reset_watchdog_error_case:
+    ; recreate "do 5 tries then give up" error handling case here.
+    ; there's a chance that hwinit randomly fails with error 0101
+    ; so it's better to retry in case of false positives
+    inc g_boot_attempts
+    mov a,g_boot_attempts
+    cjne a,#5,reset_via_watchdog
+
     acall on_reset_watchdog_timeout
     ljmp msftsmc_sysreset_watchdog_raise_rrod_and_give_up
 
